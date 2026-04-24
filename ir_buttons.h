7f03a34286cb3b00c26ddbb3703226da745d5ca3
BakiDag <73464615+BakiDag@users.noreply.github.com>
2026-04-24 18:47:14 +0200

    Add header file for IR button declarations

---
+++ ir_buttons.h
@@ -0,0 +1,15 @@
+#ifndef IR_BUTTONS_H
+#define IR_BUTTONS_H
+
+// Deklariere die IRbutton-Variablen, die in ILIFERobot.ino definiert sind
+extern const IRbutton rStart;
+extern const IRbutton rUp;
+extern const IRbutton rRight;
+extern const IRbutton rDown;
+extern const IRbutton rLeft;
+extern const IRbutton rSpot;
+extern const IRbutton rHome;
+extern const IRbutton rEdge;
+// ggf. weitere vorhandene IRbutton-Namen - füge hier alle ein, die du in ILIFERobot.ino hast
+
+#endif // IR_BUTTONS_H
