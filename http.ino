06c273124e6fd5389816e15b52a504f9ce89f5ca
BakiDag <73464615+BakiDag@users.noreply.github.com>
2026-04-24 18:41:41 +0200

    Add battery and status endpoints to HTTP server

--- http.ino
+++ http.ino
@@ -28,18 +28,59 @@ void setupHTTP() {
 //  MDNS.begin(mDNSname);
   
   httpUpdater.setup(&server, update_path, update_username, update_password);
-  server.begin();
-
-//  MDNS.addService("http", "tcp", 80);
-  Serial.printf("\n[HTTP] HTTPUpdateServer ready! Open http://%s%s in your browser and login with username '%s' and password '%s'\n", WiFi.localIP().toString().c_str(), update_path, update_username, update_password);
-  Serial.println("");
   
   server.on("/", [](){
     server.send_P(200, "text/html", PAGE_index);
   });
-  
+  server.on("/version", [](){
+  server.send(200, "text/plain", FIRMWARE_VERSION);
+});
   server.on("/cmd", HTTP_GET, processCmdRequest);
+  
+  server.on("/battery", HTTP_GET, []() {
+    float voltage = calcBattery(false);
+    float percent = calcBattery(true);
+    String json = "{\"voltage\":" + String(voltage, 2) + ",\"percent\":" + String(percent, 0) + "}";
+    server.send(200, "application/json", json);
+  });
+  server.on("/status", HTTP_GET, []() {
+  String statusText = getStatusText();
+  float voltage = calcBattery(false);
+  float percent = calcBattery(true);
+  int adcRaw = analogRead(A0);
+  
+  String json = "{";
+  json += "\"status\":\"" + statusText + "\",";
+  json += "\"Battery_Voltage\":" + String(voltage, 2) + ",";
+  json += "\"Battery_Level\":" + String(percent, 0) + ",";
+  json += "\"ADC_Raw\":" + String(adcRaw);
+  json += "}";
+  
+  server.send(200, "application/json", json);
+});
+  
+  server.begin();
+//  MDNS.addService("http", "tcp", 80);
+  Serial.printf("\n[HTTP] HTTPUpdateServer ready! Open http://%s%s in your browser and login with username '%s' and password '%s'\n", WiFi.localIP().toString().c_str(), update_path, update_username, update_password);
+  Serial.println("");
 }
+
+//void setupHTTP() {
+//  MDNS.begin(mDNSname);
+  
+  //httpUpdater.setup(&server, update_path, update_username, update_password);
+  //server.begin();
+
+//  MDNS.addService("http", "tcp", 80);
+  //Serial.printf("\n[HTTP] HTTPUpdateServer ready! Open http://%s%s in your browser and login with username '%s' and password '%s'\n", WiFi.localIP().toString().c_str(), update_path, update_username, update_password);
+  //Serial.println("");
+  
+  //server.on("/", [](){
+    //server.send_P(200, "text/html", PAGE_index);
+  //});
+  
+  //server.on("/cmd", HTTP_GET, processCmdRequest);
+//}
 
 
 
